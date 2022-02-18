8b224128bc1fa53d183a4aaba245becf45c32772
flyinggorilla <flyinggorilla@outlook.com>
2022-02-18 15:29:32 +0100

    maximet fields

--- main/Maximet.h
+++ main/Maximet.h
@@ -6,21 +6,60 @@
 #include "DataQueue.h"
 #include "Serial.h"
 
+/*static const char *const FIELDS_GMX501GPS[] = {"USERINF", "SPEED", "GSPEED", "AVGSPEED", "DIR", "GDIR", "AVGDIR", "CDIR", "AVGCDIR", "COMPASSH",
+                                               "PASL", "PSTN", "RH", "AH", "TEMP", "SOLARRAD", "XTILT", "YTILT", "ZORIENT", "STATUS", "WINDSTAT",
+                                               "GPSLOCATION", "GPSSTATUS", "TIME", "CHECK"}; */
 
-
-
-
-class Maximet {
+class Maximet
+{
 public:
-    enum Model {
-        NONE       = 0,
-        GMX200GPS  = 2001,
-        GMX501     = 5010,
-        GMX501GPS  = 5011,
+    enum Model
+    {
+        NONE = 0,
+        GMX200GPS = 2001,
+        GMX501 = 5010,
+        GMX501GPS = 5011,
         GMX501RAIN = 5012
     };
 
-    class Config {
+    enum Fields
+    {
+        USERINF, TIME, STATUS, WINDSTAT, 
+        SPEED, GSPEED, AVGSPEED, CSPEED, CGSPEED, AVGCSPEED,
+        DIR, GDIR, AVGDIR, CDIR, CGDIR, AVGCDIR,
+        TEMP, SOLARRAD, PASL, PSTN, RH, AH, 
+        COMPASSH, XTILT, YTILT, ZORIENT,
+        GPSLOCATION, GPSSTATUS, GPSSPEED, GPSHEADING, 
+        CHECK
+    };
+
+    static constexpr const char* FieldNames[] = {
+        "USERINF", "TIME", "STATUS", "WINDSTAT", 
+        "SPEED", "GSPEED", "AVGSPEED", "CSPEED", "CGSPEED", "AVGCSPEED",
+        "DIR", "GDIR", "AVGDIR", "CDIR", "CGDIR", "AVGCDIR",
+        "TEMP", "SOLARRAD", "PASL", "PSTN", "RH", "AH", 
+        "COMPASSH", "XTILT", "YTILT", "ZORIENT",
+        "GPSLOCATION", "GPSSTATUS", "GPSSPEED", "GPSHEADING", 
+        "CHECK"
+    };
+
+    static const char* GetFieldName(Fields field) { return FieldNames[field]; };
+
+
+    static constexpr Fields FIELDS_GMX501GPS[] = {USERINF, SPEED, GSPEED, AVGSPEED, DIR, GDIR, AVGDIR, CDIR, AVGCDIR,
+                                                  COMPASSH, PASL, PSTN, RH, AH, TEMP, SOLARRAD, XTILT, YTILT, ZORIENT,
+                                                  STATUS, WINDSTAT, GPSLOCATION, GPSSTATUS, TIME, CHECK};
+
+    static constexpr Fields FIELDS_GMX200GPS[] = {USERINF, GPSLOCATION, GPSSPEED, GPSHEADING, CSPEED, CGSPEED, AVGCSPEED,
+                                                  SPEED, GSPEED, AVGSPEED, DIR, GDIR, AVGDIR, CDIR, CGDIR, AVGCDIR, COMPASSH,
+                                                  XTILT, YTILT, ZORIENT, STATUS, WINDSTAT, GPSSTATUS, TIME, CHECK};
+
+    static constexpr Fields FIELDS_GMX501[] = {USERINF, SPEED, GSPEED, AVGSPEED, DIR, GDIR, AVGDIR, CDIR, AVGCDIR,
+                                               COMPASSH, PASL, PSTN, RH, AH, TEMP, SOLARRAD, XTILT, YTILT, ZORIENT,
+                                               STATUS, WINDSTAT, CHECK};
+
+    class Config
+    {
     public:
         String sReport;
         String sUserinfo;
@@ -32,19 +71,19 @@ public:
         float fCompassdecl = 0;
         float fHastn = 0;
         float fHasl = 0;
-        float fLat = 0; 
-        float fLong = 0; 
+        float fLat = 0;
+        float fLong = 0;
     };
 
 public:
-	Maximet(DataQueue &dataQueue);
-	virtual ~Maximet();
+    Maximet(DataQueue &dataQueue);
+    virtual ~Maximet();
 
     // start the task
     void Start(int gpioRX, int gpioTX, bool alternateUart = false);
 
     // stop the task (e.g. before OTA update)
-    void Stop() { mbRun = false; }; 
+    void Stop() { mbRun = false; };
 
     // weatherbuoy electronics can be used to simulator a Gill Maximet Weatherstation via its RS232 serial interface. good for testing other weatherbuoys
     void SimulatorStart(Model maximetModel);
@@ -53,27 +92,26 @@ public:
 
     void SimulatorDataPoint(float temperature, double longitude, double latitude);
 
-    Config& GetConfig() { return mMaximetConfig; };
+    Config &GetConfig() { return mMaximetConfig; };
 
 private:
-    //main loop run by the task
+    // main loop run by the task
     void MaximetTask();
     friend void fMaximetTask(void *pvParameter);
     void MaximetConfig();
 
-    void SendLine(const char* text);
+    void SendLine(const char *text);
     void SendLine(String &line);
 
-
-    // enters Maximet commmandline mode with *\r\n and allows to send config; 
+    // enters Maximet commmandline mode with *\r\n and allows to send config;
     // calls "exit" automatically at the end to continue data sending
     bool EnterCommandLine();
     void ExitCommandLine();
     bool ReadConfig(String &value, const char *sConfig);
     bool WriteConfig(const char *sConfig, const String &value);
-    bool ReadConfigFloat(float &val, const char* cmd);
-    bool ReadConfigString(String &val, const char* cmd);
-    bool ReadConfigInt(int &val, const char* cmd);
+    bool ReadConfigFloat(float &val, const char *cmd);
+    bool ReadConfigString(String &val, const char *cmd);
+    bool ReadConfigInt(int &val, const char *cmd);
 
     bool ReadUserinf();
     bool ReadSensor();
@@ -102,18 +140,18 @@ private:
     // set longitude if not GPS
     void WriteLong(float lon);
 
-    // configure Maximet long average interval - 
+    // configure Maximet long average interval -
     // default is 10 minutes (10 times short interval of default 60 seconds)
     void WriteAvgLong(unsigned short avglong);
 
     // reconfigure Maximet output frequency
     // high = true: 1 output per second
     // high = false: 1 output per minute
-    void WriteOutfreq(bool high);  
+    void WriteOutfreq(bool high);
     // set columns maximet should send e.g. "USERINF,SPEED,GSPEED,AVGSPEED,DIR" --- this will send "REPORT USERINF SPEED .... " (no comma!) to maximet
-    void WriteReport(const char* report);
+    void WriteReport(const char *report);
     // set
-    void WriteUserinf(const char* userinf);
+    void WriteUserinf(const char *userinf);
 
     Model mMaximetModel;
     Serial *mpSerial;
@@ -123,7 +161,7 @@ private:
 
     DataQueue &mrDataQueue;
     Config mMaximetConfig;
-    
+
     bool mbRun = true;
     bool mbCommandline = false;
 
@@ -131,6 +169,4 @@ private:
     unsigned int muiSolarradiation = 999;
 };
 
-#endif 
-
-
+#endif
